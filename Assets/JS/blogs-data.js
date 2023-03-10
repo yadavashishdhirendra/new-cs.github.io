05211896ec8fc9e65a49d72d92ac000dabade1ea
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2023-03-10 16:07:32 +0530

    added imh

--- Assets/JS/blogs-data.js
+++ Assets/JS/blogs-data.js
@@ -4,7 +4,7 @@ var template = Handlebars.compile(source);
 var data = {
     users: [ 
         {
-            imgs: "https://i.ytimg.com/vi/qqdutiOJvKQ/maxresdefault.jpg",
+            imgs: "https://yadavashishdhirendra.github.io/new-cs.github.io/Assets/Images/00004.jpg",
             Title: "Personal Care",
             uploadDate: "20-02-2023",
             Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
@@ -13,29 +13,14 @@ var data = {
 
         },
         {
-            imgs: "https://i.ytimg.com/vi/2SrHOuKvchs/maxresdefault.jpg",
+            imgs: "https://yadavashishdhirendra.github.io/new-cs.github.io/Assets/Images/00002.jpg",
             Title: "Health Care",
             uploadDate: "18-02-2023",
             Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
             urls: "#",
             link: "#"
         },
-        {
-            imgs: "https://i.ytimg.com/vi/2SrHOuKvchs/maxresdefault.jpg",
-            Title: "Body Care",
-            uploadDate: "18-02-2023",
-            Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
-            urls: "#",
-            link: "#"
-        },
-        {
-            imgs: "https://i.ytimg.com/vi/2SrHOuKvchs/maxresdefault.jpg",
-            Title: "Agro Care",
-            uploadDate: "18-02-2023",
-            Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
-            urls: "#",
-            link: "#"
-        },
+    
     ]
 };
 
