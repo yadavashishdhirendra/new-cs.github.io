c1a3ef1340af7b0a3c617e3ac448eede15c12337
yadavashishdhirendra <85459137+DiTechCDM@users.noreply.github.com>
2023-03-12 20:16:09 +0530

    added

--- Assets/JS/blogs-data.js
+++ Assets/JS/blogs-data.js
@@ -5,18 +5,18 @@ var data = {
     users: [ 
         {
             imgs: "https://yadavashishdhirendra.github.io/new-cs.github.io/Assets/Images/00004.jpg",
-            Title: "Personal Care",
+            Title: "A Comprehensive Guide to Understanding and Succeeding in Direct Selling",
             uploadDate: "20-02-2023",
-            Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
+            Desc: "Direct Selling involves making sales directly to customers without the assistance of any middlemen. It is the practice of selling goods outside of a traditional retail environment, such as online, at home, or in other locations. Wholesalers and local distribution hubs are two examples of the middlemen it gets rid of in the distribution process.",
             urls: "#",
             link: "#"
 
         },
         {
             imgs: "https://yadavashishdhirendra.github.io/new-cs.github.io/Assets/Images/00002.jpg",
-            Title: "Health Care",
+            Title: "The Direct Selling Industry: A Global Perspective",
             uploadDate: "18-02-2023",
-            Desc: "BhuAstra Super is an organic solution for reducing very tough and recurrent crop damages due to soil-borne insects, pests like white grub, crickets, cutworms, snails, termites.",
+            Desc: "Direct selling is the practice of promoting and selling a product or service directly to a consumer outside of a traditional retail setting (like a customer's home, place of business, etc.) through personal explanation and demonstration. It's been around for a long time and is very much like the standard retail setup for everyday items, making it one of the earliest forms of commerce.",
             urls: "#",
             link: "#"
         },
